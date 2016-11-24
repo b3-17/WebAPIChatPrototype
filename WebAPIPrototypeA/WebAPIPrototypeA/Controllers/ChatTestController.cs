ce665a4aca8bde3e1cd2d18a6821ad1820980d74
joe <you@example.com>
2016-11-24 13:27:00 -0800

    add json response and testing for getting all channels

---
+++ WebAPIPrototypeA/WebAPIPrototypeA/Controllers/ChatTestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAPIPrototypeA.Controllers
+{
+    public class ChatTestController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+    }
+}
